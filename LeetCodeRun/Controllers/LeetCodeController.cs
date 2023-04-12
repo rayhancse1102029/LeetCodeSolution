6816a1361f79d43a9b92fe27c0127fd50fd188fa
rayhancse1102029 <rayhan.cse15@gmail.com>
2023-04-12 23:52:37 +0600

    Add project files.

---
+++ LeetCodeRun/Controllers/LeetCodeController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeetCodeRun.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeetCodeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Execution")]
+        public async Task<IActionResult> Execution()
+        {
+            /*====================Contains Duplicate==============*/
+            //int[] nums = { 0 };
+            //var response = ContainsDuplicate(nums);
+
+            /*==============Two Sum=================*/
+            int[] nums = { 3, 3 };
+            var response = TwoSum(nums, 6);
+
+
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("ContainsDuplicate")]
+        public bool ContainsDuplicate(int[] nums)
+        {
+
+            int len = nums.Length;
+            //int?[] numList = new int?[len];
+            HashSet<int?> numList = new HashSet<int?>();
+
+            for (int i = 0; i < len; i++)
+            {
+                if (numList.Contains(nums[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    numList.Add(nums[i]);
+                }
+            }
+
+            return false;
+        }
+
+        [HttpPost]
+        [Route("TwoSum")]
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int len = nums.Length; int lookingFor = 0;
+            int[] resultList = new int[2];
+
+            for (int i = 0; i < len; i++)
+            {
+                lookingFor = target - nums[i];
+
+                int index = Array.IndexOf(nums, lookingFor);
+                if (index < 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (i == index)
+                    {
+                        //index = i - 1 > 0 ? Array.IndexOf(nums, lookingFor, 0, i-1) :
+                        //    (index < 0 || i == index) ? Array.IndexOf(nums, lookingFor,i < len ? i + 1 : i, i==len ? len - 1 : len) : -1;
+
+                        if(i - 1 > 0)
+                        {
+                            index = Array.IndexOf(nums, lookingFor, 0, i - 1);
+                        }
+                        else if(index < 0 || i == index)
+                        {
+                            index = Array.IndexOf(nums, lookingFor, i < len ? i + 1 : i, len - 1);
+                        }
+                        else
+                        {
+                            index = -1;
+                        }
+
+                        //index = index < 0 || i == index ? Array.IndexOf(nums, lookingFor, i + 1, len-1) : 0;
+                    }
+
+                    if(index < 0)
+                    {
+                        continue;
+                    }
+
+                    if (i < index)
+                    {
+                        resultList[0] = i;
+                        resultList[1] = index;
+                        break;
+                    }
+                    else
+                    {
+                        resultList[0] = index;
+                        resultList[1] = i;
+                        break;
+                    }
+                }
+               
+            }
+            return resultList;
+        }
+    }
+}
